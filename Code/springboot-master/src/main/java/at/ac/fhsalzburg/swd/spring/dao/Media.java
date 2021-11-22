23611097850b80f7f003fd4e5889ed6dcbc965f3
m-langer <85168282+m-langer@users.noreply.github.com>
2021-11-22 10:47:26 +0100

    add media

--- Code/springboot-master/src/main/java/at/ac/fhsalzburg/swd/spring/dao/Media.java
+++ Code/springboot-master/src/main/java/at/ac/fhsalzburg/swd/spring/dao/Media.java
@@ -1,5 +1,30 @@
 package at.ac.fhsalzburg.swd.spring.dao;
 
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Media {
-    private int iD;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String author;
+    private mediaType type;
+    private String ISBN;
+    private String FSK;
+    private int length;
+    private category category;
+
+    public enum mediaType {
+        simpleBook, specializedBook, movie, movieAdult, cd
+    }
+
+    public enum category {
+        maths, physics, biology, thriller, fantasy, romance, action
+    }
 }
